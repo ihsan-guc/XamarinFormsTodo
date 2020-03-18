2f98e98ac517d6cdb1416cea7dc5632704c8837a
ihsan-guc <47850974+ihsan-guc@users.noreply.github.com>
2020-03-18 22:18:57 +0300

    Image add

--- XamarinFormsTodo.Android/obj/Debug/90/designtime/Resource.designer.cs
+++ XamarinFormsTodo.Android/obj/Debug/90/designtime/Resource.designer.cs
@@ -5795,13 +5795,19 @@ namespace XamarinFormsTodo.Droid
 			public const int notify_panel_notification_icon_bg = 2131165295;
 			
 			// aapt resource value: 0x7F070070
-			public const int plus = 2131165296;
+			public const int picture = 2131165296;
 			
 			// aapt resource value: 0x7F070071
-			public const int tooltip_frame_dark = 2131165297;
+			public const int picture2 = 2131165297;
 			
 			// aapt resource value: 0x7F070072
-			public const int tooltip_frame_light = 2131165298;
+			public const int plus = 2131165298;
+			
+			// aapt resource value: 0x7F070073
+			public const int tooltip_frame_dark = 2131165299;
+			
+			// aapt resource value: 0x7F070074
+			public const int tooltip_frame_light = 2131165300;
 			
 			static Drawable()
 			{
